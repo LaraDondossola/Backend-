0aa669ffe71c8809845f51f238669158687dcbf3
Lara <laradrdondossola@gmail.com>
2025-08-29 12:54:53 -0300

    add lista005

---
+++ Lista005/ex5/src/main/java/org/example/Filme.java
@@ -0,0 +1,11 @@
+public class Filme {
+    public String nome;
+    public double preco;
+    public int idadeMinima;
+
+    public Filme(String nome, double preco, int idadeMinima) {
+        this.nome = nome;
+        this.preco = preco;
+        this.idadeMinima = idadeMinima;
+    }
+}
